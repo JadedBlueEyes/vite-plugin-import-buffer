0316d6fefd8fbc3b60c377c62707267f6c4cad95
Jade Ellis <jade@ellis.link>
2025-02-15 23:10:48 +0000

    build: improve rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,26 @@
 // rollup.config.js
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
-import pkg from './package.json' with { type: "json" };
+import pkg from "./package.json" with { type: "json" };
+import { isBuiltin } from "node:module";
+
+const external = (id) =>
+	isBuiltin(id) || pkg.dependencies[id] || pkg.peerDependencies[id];
 
 export default [
-    {
-        input: 'src/index.ts',
-        output: [
-            { file: pkg.exports.require, format: 'cjs' },
-            { file: pkg.exports.import, format: 'es' }
-        ],
-        plugins: [typescript()]
-    },
-    {
-        input: 'src/index.ts',
-        output: [{ file: pkg.exports.types, format: 'es' }],
-        plugins: [dts()],
-    }
+	{
+		input: "src/index.ts",
+		output: [
+			{ file: pkg.exports.require, format: "cjs" },
+			{ file: pkg.exports.import, format: "es" },
+		],
+		plugins: [typescript()],
+		external,
+	},
+	{
+		input: "src/index.ts",
+		output: [{ file: pkg.exports.types, format: "es" }],
+		plugins: [dts()],
+		external,
+	},
 ];
